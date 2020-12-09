1e6fc2653952849b988a9f31fa66acbc4388f15f
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-12-09 22:45:31 +0200

    Create some Marks

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -1,9 +1,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties:
 
+    // MARK: Starting Here:
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    // MARK: Methods:
+    
 }
 
