ed18d92b06a36b2ccdb8aa0ce50bef8e1c674f86
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-12-09 21:39:46 +0200

    Create Outlets for all UI elements

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -1,7 +1,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var numberButtons: [UIButton]!
+    @IBOutlet var mathActionButtons: [UIButton]!
+    @IBOutlet weak var dotButton: UIButton!
+    @IBOutlet weak var acButton: UIButton!
+    @IBOutlet weak var plusMinusButton: UIButton!
+    @IBOutlet weak var percentButton: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
