167db86de05f3060b65bb779fedc128956a813b3
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-12-09 22:45:44 +0200

    Create outlets and actions

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -2,6 +2,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK: Outlets:
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet var allButtons: [UIButton]!
+    
     // MARK: Properties:
 
     // MARK: Starting Here:
@@ -11,5 +16,8 @@ class ViewController: UIViewController {
     
     // MARK: Methods:
     
+    // MARK: Actions:
+    @IBAction func someButtonTapped(_ sender: UIButton) {
+    }
 }
 
