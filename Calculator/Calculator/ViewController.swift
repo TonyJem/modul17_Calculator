d2ee5890d7e1c864144849d2419786c2e575b8e5
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-12-09 20:26:56 +0200

    Create New Project in xCode

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Office-iMac on 2020-12-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
