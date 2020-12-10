86e9607c816a9810e5965a9ef35a5aee182b2247
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-12-11 00:05:37 +0200

    Add Button Text setup

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,6 +9,8 @@ class ViewController: UIViewController {
     
     // MARK: Properties:
     private let appBackgroundColor: UIColor = .black
+    private let fontColor: UIColor = .blue
+    private let fontSize: CGFloat = 32
     
     private let allPrimaryButtonTags = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     private let allActionButtonTags = [11, 12, 13, 14, 15]
@@ -40,6 +42,8 @@ class ViewController: UIViewController {
             if allSecondaryButtonTags.contains(button.tag) {
                 button.backgroundColor = colorSchemeForSecondaryButtons.buttonBackgroundColor
             }
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: fontSize)
+        button.setTitleColor(fontColor, for: .normal)
         }
         
     }
