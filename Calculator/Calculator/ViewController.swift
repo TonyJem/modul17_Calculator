f6e1fb508851747741c38bc24e75cd4070e5db21
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-12-10 23:45:36 +0200

    Create setup for Button UI

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -8,17 +8,45 @@ class ViewController: UIViewController {
     @IBOutlet var allButtons: [CalcButton]!
     
     // MARK: Properties:
+    private let appBackgroundColor: UIColor = .black
     
-
+    private let allPrimaryButtonTags = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    private let allActionButtonTags = [11, 12, 13, 14, 15]
+    private let allSecondaryButtonTags = [16, 17, 18]
+    
+    private let colorSchemeForPrimaryButtons: ButtonColorScheme = .primaryButton
+    private let colorSchemeForActionButtons: ButtonColorScheme = .actionButton
+    private let colorSchemeForSecondaryButtons: ButtonColorScheme = .secondaryButton
+    
     // MARK: Starting Here:
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
     }
     
     // MARK: Methods:
+    private func setupUI() {
+        backView.backgroundColor = appBackgroundColor
+        
+        for button in allButtons {
+            if allPrimaryButtonTags.contains(button.tag) {
+                button.backgroundColor = colorSchemeForPrimaryButtons.buttonBackgroundColor
+            }
+            
+            if allActionButtonTags.contains(button.tag) {
+                button.backgroundColor = colorSchemeForActionButtons.buttonBackgroundColor
+            }
+            
+            if allSecondaryButtonTags.contains(button.tag) {
+                button.backgroundColor = colorSchemeForSecondaryButtons.buttonBackgroundColor
+            }
+        }
+        
+    }
     
     // MARK: Actions:
     @IBAction func someButtonTapped(_ sender: CalcButton) {
     }
+    
+    
 }
-
