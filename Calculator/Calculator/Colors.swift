4e46a69b0760e9266d1df1492c52cdf658a08c6c
TonyJem <a.jemanov@gmail.com>
2020-12-24 22:19:12 +0200

    Make code nicer in "Colors.swift"

--- Calculator/Calculator/Colors.swift
+++ Calculator/Calculator/Colors.swift
@@ -9,8 +9,8 @@ struct Colors {
     static let buttonFontColor: UIColor = .blue
     static let buttonBorderColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
     
-    static let actionButtonNormal: UIColor = .orange
-    static let actionButtonFrozen: UIColor = UIColor(red: 239/255, green: 94/255, blue: 28/255, alpha: 1.0)
     static let primaryButton: UIColor = .cyan
     static let secondaryButton: UIColor = .lightGray
+    static let actionButtonNormal: UIColor = .orange
+    static let actionButtonFrozen: UIColor = UIColor(red: 239/255, green: 94/255, blue: 28/255, alpha: 1.0)
 }
