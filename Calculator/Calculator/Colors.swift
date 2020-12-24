193a7fb0a7be650dfd0199b719bd83e91dd9e660
TonyJem <a.jemanov@gmail.com>
2020-12-24 21:34:53 +0200

    Refactor "buttonBorderColor"

--- Calculator/Calculator/Colors.swift
+++ Calculator/Calculator/Colors.swift
@@ -7,7 +7,7 @@ struct Colors {
     static let labelFontColorFrozen: UIColor = .gray
     
     static let buttonFontColor: UIColor = .blue
-    static let buttonBorderColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1.0)
+    static let buttonBorderColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
     
     static let actionButtonNormal: UIColor = .orange
     static let actionButtonFrozen: UIColor = UIColor(red: 239/255, green: 94/255, blue: 28/255, alpha: 1.0)
