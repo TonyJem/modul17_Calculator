ce8c295e47a7b609a00caba9095f8840b00af860
TonyJem <a.jemanov@gmail.com>
2020-12-24 23:39:14 +0200

    Update CalcLabelDTO.swift

--- Calculator/Calculator/CalcLabelDTO.swift
+++ Calculator/Calculator/CalcLabelDTO.swift
@@ -1,1 +1,23 @@
 import UIKit
+
+struct CalcLabelDTO {
+    static var resultLabelActive: CalcLabelDTO {
+        return CalcLabelDTO()
+    }
+    
+    static var resultLabelNotActive: CalcLabelDTO {
+        return CalcLabelDTO()
+    }
+  
+    init(fontSize: CGFloat = 65,
+         fontColor: UIColor = Colors.labelFontColorNormal,
+         backgroundColor: UIColor = Colors.applicationBackgroundColor) {
+        self.fontSize = fontSize
+        self.fontColor = fontColor
+        self.backgroundColor = backgroundColor
+    }
+
+    let fontSize: CGFloat
+    let fontColor: UIColor
+    let backgroundColor: UIColor
+}
